9eada7ee0c5fa32e58af2b89c898c9576297a751
Ruwana_112478 <ruwana.amarasinghe@axiatadigitallabs.com>
2025-04-29 09:45:32 +0530

    feat: delete button function

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,9 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react"
+import {faPenToSquare, faTrash} from "@fortawesome/free-solid-svg-icons";
 
-export const ToDo = () => {
+export const ToDo = ({task, toggleComplete, deleteTodo}) => {
     return (
-        <div>
+        <div className="Todo">
+            <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed': ""}
+            `}>{task.task}</p>
+            <div>
+                <FontAwesomeIcon icon={faPenToSquare}/>
+                <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)}/>
+            </div>
         </div>
     )
 }
--- src/components/ToDoWrapper.js
+++ src/components/ToDoWrapper.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react"
 import ToDoForm from "./ToDoForm";
 import {v4 as uuidv4} from 'uuid';
+import ToDo from "./ToDo";
+import toDo from "./ToDo";
 uuidv4();
 
 export const ToDoWrapper = () => {
@@ -12,9 +14,20 @@ export const ToDoWrapper = () => {
         }])
         console.log(todos)
     }
+    const toggleComplete = id => {
+        setTodos(todos.map(todo => todo.id === id ? {
+            todo, completed: !todo.completed} : todo))
+    }
+    const deleteTodo = id => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
     return (
         <div className="TodoWrapper">
+            <h1>Get Things Done</h1>
             <ToDoForm addTodo={addTodo}/>
+            {todos.map((todo, index) => (
+                <ToDo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo}/>
+            ))}
         </div>
     )
 }
